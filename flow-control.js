ebd10482778b6cbbfce84878d635058ae1dcaf2c
Azamat Akmatov <ackmatoff@gmail.com>
2020-02-07 13:04:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,7 +12,7 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   if (ternaryTeenager >= 13 <= 19) {
-    return "You are a teenager";
+    return "You are a teenager!";
   } else {
     return "You are not a teenager";
   }
